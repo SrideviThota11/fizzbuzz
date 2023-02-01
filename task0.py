5e7280a67c192653149fca1204e04693a29b2772
Sridevi Thota <sr443603@dal.ca>
2023-02-02 05:03:34 +0530

    I made a commit

---
+++ task0.py
@@ -0,0 +1,12 @@
+n=100
+i=1
+for i in range(1,n+1):
+    if(i%3!=0 and i%5!=0): #not multiples of 3 and 5
+        print(i)
+    elif(i%3==0 and i%5==0): #multiples of 3 and 5
+        print("FizzBuzz")
+    elif(i%3==0):            #multiples of 3
+        print("Fizz")
+    else:                    #multiples of 5
+        if(i%5==0):
+            print("Buzz")
